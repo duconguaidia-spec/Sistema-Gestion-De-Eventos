7d5df50a1ffe1d62c32d7ecd96baf212bbbaad3e
mauricioponguta <pongutaerin18@gmail.com>
2026-06-04 15:00:45 -0500

    se crea Respuestas JSON estandarizadas

--- Api_Crud_Mauricio/api/views.py
+++ Api_Crud_Mauricio/api/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 
 from .models import (
@@ -22,50 +21,51 @@ from .serializers import (
     ConferenciaSerializer,
     PatrocinadorSerializer,
 )
+from .responses import StandardResponseMixin
 
 # Crud de tabla Sede (GET, POST, PUT, DELETE)
-class SedeViewSet(viewsets.ModelViewSet):
+class SedeViewSet(StandardResponseMixin, viewsets.ModelViewSet):
     queryset = Sede.objects.all()
     serializer_class = SedeSerializer
 
 
 # Crud de tabla Organizador (GET, POST, PUT, DELETE)
-class OrganizadorViewSet(viewsets.ModelViewSet):
+class OrganizadorViewSet(StandardResponseMixin, viewsets.ModelViewSet):
     queryset = Organizador.objects.all()
     serializer_class = OrganizadorSerializer
 
 
 # Crud de tabla Evento (GET, POST, PUT, DELETE)
-class EventoViewSet(viewsets.ModelViewSet):
+class EventoViewSet(StandardResponseMixin, viewsets.ModelViewSet):
     queryset = Evento.objects.all()
     serializer_class = EventoSerializer
 
 
 # Crud de tabla Asistente (GET, POST, PUT, DELETE)
-class AsistenteViewSet(viewsets.ModelViewSet):
+class AsistenteViewSet(StandardResponseMixin, viewsets.ModelViewSet):
     queryset = Asistente.objects.all()
     serializer_class = AsistenteSerializer
 
 
 # Crud de tabla Inscripcion (GET, POST, PUT, DELETE)
-class InscripcionViewSet(viewsets.ModelViewSet):
+class InscripcionViewSet(StandardResponseMixin, viewsets.ModelViewSet):
     queryset = Inscripcion.objects.all()
     serializer_class = InscripcionSerializer
 
 
 # Crud de tabla Pago (GET, POST, PUT, DELETE)
-class PagoViewSet(viewsets.ModelViewSet):
+class PagoViewSet(StandardResponseMixin, viewsets.ModelViewSet):
     queryset = Pago.objects.all()
     serializer_class = PagoSerializer
 
 
 # Crud de tabla Conferencia (GET, POST, PUT, DELETE)
-class ConferenciaViewSet(viewsets.ModelViewSet):
+class ConferenciaViewSet(StandardResponseMixin, viewsets.ModelViewSet):
     queryset = Conferencia.objects.all()
     serializer_class = ConferenciaSerializer
 
 
 # Crud de tabla Patrocinador (GET, POST, PUT, DELETE)
-class PatrocinadorViewSet(viewsets.ModelViewSet):
+class PatrocinadorViewSet(StandardResponseMixin, viewsets.ModelViewSet):
     queryset = Patrocinador.objects.all()
     serializer_class = PatrocinadorSerializer
--- Api_Crud_Mauricio/backend/settings.py
+++ Api_Crud_Mauricio/backend/settings.py
@@ -128,3 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+REST_FRAMEWORK = {
+    'EXCEPTION_HANDLER': 'api.responses.standard_exception_handler',
+}
